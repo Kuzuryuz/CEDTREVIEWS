a7d1faa5f4798f4f80584dcf37b8462e336d6713
Kuzuryu <71879159+Kuzuryuz@users.noreply.github.com>
2023-10-06 15:55:24 +0700

    change header to responsive

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ document.getElementById('review-button').addEventListener('click', () => {
     document.getElementById('review-page').style.display = 'block';
 });
 
-document.getElementById('close-button').addEventListener('click', () => {
+document.getElementById('close-review-button').addEventListener('click', () => {
     document.getElementById('review-page').style.display = 'none';
 });
